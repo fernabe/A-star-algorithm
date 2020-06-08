3e8524ee9b120ef0089422662767a4eb1a287ecd
fernabe <fer.barrabes@gmail.com>
2020-06-08 21:21:12 +0200

    generate maze automatically

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,22 @@
 #include "path.h"
 
 int main()
-{
-    
-    std::vector<std::vector<int>> maze{
-        {0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-    };
+{   
+    constexpr int MAZE_SIZE{10};
 
+    std::vector<std::vector<int>> maze;
+    for(size_t i = 0; i < MAZE_SIZE ; i++)
+    {
+        std::vector<int> vec;
+        for(size_t j = 0; j < MAZE_SIZE ; j++)
+        {
+            int n = rand()%(1 - 0 + 1);
+            vec.push_back(n);
+           
+        }
+        maze.push_back(vec);
+    }
+    
     Node *start = new Node(nullptr, 0, 0);
     Node *end = new Node(nullptr, 1, 5);
 
